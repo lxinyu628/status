db887f830aa14e7d687f0e3f740666248f9af2fa
geeks <geeks@88vip.com>
2024-08-20 20:42:16 +0800

    update

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -13,35 +13,22 @@ const Footer = () => {
   return (
     <footer id="footer">
       <div className="social">
-        <CustomLink
-          iconDom={<GithubOne />}
-          to={`https://github.com/${githubName}/`}
-        />
         <CustomLink iconDom={<Home />} to={homeUrl} />
-        <CustomLink iconDom={<Mail />} to={`mailto:${emailUrl}`} />
       </div>
       <div className="text">
         <p>
-          <CustomLink
-            text={Package.alia}
-            to="https://github.com/imsyy/site-status"
-          />
-          &nbsp;Version&nbsp;{Package.version}
-        </p>
-        <p>
-          基于&nbsp;
+          基于 
           <CustomLink to="https://uptimerobot.com/" text="UptimeRobot" />
-          &nbsp;接口&nbsp;|&nbsp;检测频率 5 分钟
+           接口 | 检测频率 5 分钟
         </p>
         <p>
-          Copyright&nbsp;&copy;&nbsp;2020&nbsp;-&nbsp;{new Date().getFullYear()}
-          &nbsp;
-          <CustomLink to="https://www.imsyy.top/" text="無名" />
+          Copyright © 2022 - {new Date().getFullYear()}
+           
+          <CustomLink to="https://www.geekfunlab.com/" text="极客趣味工作室" />
           {siteIcp ? (
-            <React.Fragment>
-              &nbsp;|&nbsp;
-              <CustomLink to="https://beian.miit.gov.cn/" text={siteIcp} />
-            </React.Fragment>
+            <>
+              <CustomLink to={siteIcp} text={siteIcp} />
+            </>
           ) : null}
         </p>
       </div>
